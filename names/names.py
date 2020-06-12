402c44466222c1ff5e38bdc42dc443fcf7dd8d14
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-12 09:47:51 -0700

    get duplicated names

--- names/names.py
+++ names/names.py
@@ -66,12 +66,27 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# still bad 
+# names_dict = {}
+# for name in names_1:
+#     names_dict[name] = True
+
+names = BinarySearchTree("n")
+for name in names_1:
+    names.insert(name)
+
+for name in names_2:
+    if names.contains(name):
+        duplicates.append(name)
+
+
+
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
