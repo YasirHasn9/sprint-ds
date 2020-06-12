a64c22075fede0fcf889f93bfe0da8b7ce09503a
YasirHasn9 <hassanyasir223@gmail.com>
2020-06-12 12:16:01 -0700

    MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -28,15 +29,75 @@ class LinkedList:
         if not self.head:
             return False
 
+# at what current node are we ?
         current = self.head
 
+# valid node
         while current:
             if current.get_value() == value:
                 return True
 
+# update the current node
             current = current.get_next()
 
+# if we reached this point then we dont have a valid node
         return False
 
+#     # def reverse_list(self, node, prev):
+#     #     prev = None
+#     #     cur = self.head
+#     #     while cur:
+#     #         next_head = cur.next
+#     #         cur.next = None
+#     #         prev = cur
+#     #         cur = next_head
+#     #     self.head = prev
+
+#         # # myList = []
+#         # # if self.head:
+#         # #     myList.append(node)
+
+#         # # return list.reverse(myList)
+#         # # check the head
+#         # # and then store the head in Temporarily variable
+#         # # get the next node
+#         # if self.head:
+#         #     # Temporarily head
+#         #     current_head = self.head
+#         #     # we need the next node value of the current_head
+#         #     next_head = current_head.get_value()
+#         #     # now we need to make the head's next to none so we
+#         #     # can swipe to the end
+
+#         #     # now the current head is floating points to nothing
+#         #     current_head.next = None
+
+#         #     # iterate over the head of the nodes
+#         #     while next_head:
+#         #         # get the pervious head
+#         #         prev = current_head
+
+#         #         # now we can make the current head to next_head
+#         #         current_head = next_head
+#         #         next_head = current_head.get_next()
+#         #         current_head.next_node = prev
+#         #         self.head = current_head
+
     def reverse_list(self, node, prev):
-        pass
+        # make previous node == None
+        prev = None
+        # get the current head
+        head = self.head
+        while head:
+            # since the head is not None
+            # make the next node the node that was the next of the current head
+            next_node = head.next_node
+
+            # now the next node needs to be None
+            head.next_node = prev
+            # make the pervious node the next head
+            prev = head
+
+            # the new head is the next node that was the previous node
+            head = next_node
+        self.head = prev
